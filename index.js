a20f00665ae9be309e74050a3a819c8fc4d50d0b
Spencer Lyons <sjl8r8@gmail.com>
2019-11-23 12:41:05 -0600

    got dealer cards working

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/* 
-Player Turn 
-Dealer Turn
-Card Class
-Card Array
-Scoring Function
-Game Rules Fucntion
-*/
-
 console.log('linked!')
 class Card {
     constructor(name, value, imgUp, imgDown, faceUp) {
@@ -18,33 +9,42 @@ class Card {
     }
 }
 
-
 //Hearts
 let aceOfHearts = new Card('Ace of Hearts', 11, "images/AH.png", "images/red_back.png", true);
 let queenOfHearts = new Card('Queen of Hearts', 10, "images/QH.png", "images/red_back.png", true);
 let tenOfHearts = new Card('Ten of Hearts', 10, "images/10H.png", "images/red_back.png", true);
 let sixOfHearts = new Card('Six Of Hearts', 6, "images/6H.png", "images/red_back.png", true);
+let threeOfHearts = new Card('Three Of Hearts', 3, "images/3H.png", "images/red_back.png", true);
+let twoOfHearts = new Card('Two Of Hearts', 2, "images/2H.png", "images/red_back.png", true);
 
 //Spades
 let aceOfSpades = new Card('Ace of Spades', 11, "images/AS.png", "images/red_back.png", true);
 let kingOfSpades = new Card('King of Spades', 10, "images/KS.png", "images/red_back.png", true);
 let nineOfSpades = new Card('Nine of Spades', 9, "images/9S.png", "images/red_back.png", true);
 let fiveOfSpades = new Card('Five of Spades', 5, "images/5S.png", "images/red_back.png", true);
+let fourOfSpades = new Card('Four of Spades', 4, "images/4S.png", "images/red_back.png", true);
+let threeOfSpades = new Card('Three of Spades', 3, "images/3S.png", "images/red_back.png", true);
 
 //Clubs
 let aceOfClubs = new Card('Ace of Clubs', 11, "images/AC.png", "images/red_back.png", true);
 let jackOfClubs = new Card('Jack of Clubs', 10, "images/JC.png", "images/red_back.png", true);
 let sevenOfClubs = new Card('Seven of Clubs', 7, "images/7C.png", "images/red_back.png", true);
+let fourOfClubs = new Card('Four of Clubs', 4, "images/4C.png", "images/red_back.png", true);
 let threeOfClubs = new Card('Three of Clubs', 3, "images/3C.png", "images/red_back.png", true);
+let twoOfClubs = new Card('Two of Clubs', 2, "images/2C.png", "images/red_back.png", true);
 
 //Diamonds
 let aceOfDiamonds = new Card('Ace of Diamonds', 11, "images/AD.png", "images/red_back.png", true);
 let eightOfDiamonds = new Card('Eight of Diamonds', 8, "images/8D.png", "images/red_back.png", true);
+let fiveOfDiamonds = new Card('Five of Diamonds', 5, "images/5D.png", "images/red_back.png", true);
 let fourOfDiamonds = new Card('Four of Diamonds', 4, "images/4D.png", "images/red_back.png", true);
+let threeOfDiamonds = new Card('Three of Diamonds', 3, "images/3D.png", "images/red_back.png", true);
 let twoOfDiamonds = new Card('Two of Diamonds', 2, "images/2D.png", "images/red_back.png", true);
 
+//Hold all the Cards with an Array
+let cardArray = [aceOfSpades, aceOfHearts, aceOfClubs, aceOfDiamonds, kingOfSpades, nineOfSpades, fiveOfSpades, fourOfSpades, threeOfSpades, queenOfHearts, tenOfHearts, sixOfHearts, threeOfHearts, twoOfHearts, jackOfClubs, sevenOfClubs, fourOfClubs, threeOfClubs, twoOfClubs, eightOfDiamonds, fiveOfDiamonds, fourOfDiamonds, threeOfDiamonds, twoOfDiamonds];
 
-let cardArray = [aceOfSpades, aceOfHearts, aceOfClubs, aceOfDiamonds, kingOfSpades, nineOfSpades, fiveOfSpades, queenOfHearts, tenOfHearts, sixOfHearts, jackOfClubs, sevenOfClubs, threeOfClubs, eightOfDiamonds, fourOfDiamonds, twoOfDiamonds];
+console.log(`cardArray length is ${cardArray.length}`)
 
 //Card Slots
 let playerCardOne = document.getElementById('playerOne');
@@ -56,8 +56,13 @@ let playerCardSix = document.getElementById('playerSix');
 let playerCardSeven = document.getElementById('playerSeven');
 let dealerCardOne = document.getElementById('dealerOne');
 let dealerCardTwo = document.getElementById('dealerTwo');
+let dealerCardThree = document.getElementById('dealerThree');
+let dealerCardFour = document.getElementById('dealerFour');
+let dealerCardFive = document.getElementById('dealerFive');
+let dealerCardSix = document.getElementById('dealerSix');
 
 //Wager, Balance and Current Hand
+let dealerHand = document.getElementById('dealerHand');
 let balanceId = document.getElementById('balanceDisplay');
 let wagerId = document.getElementById('wagerDisplay');
 let handScore = document.getElementById('handScore');
@@ -88,7 +93,7 @@ const changeWager = () => {
 const shuffleCards = (cardArr) => {
     let cardDeck = [];
     let randomNumber, card;
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 24; i++) {
         randomNumber = Math.floor(Math.random() * cardArray.length);
         card = cardArray.splice(randomNumber, 1);
         if (!cardDeck.includes(card)) {
@@ -175,6 +180,11 @@ const deal = (cardArr) => {
             playerCardFive.style.display = 'block'
             playerScore += cardSeven.value;
             handScore.innerHTML = `Current Hand: ${playerScore}`;
+        } else if (count === 4) {
+            playerCardSix.src = cardEight.imgUp;
+            playerCardSix.style.display = 'block';
+            playerScore += cardEight.value;
+            handScore.innerHTML = `Current Hand: ${playerScore}`;
         }
     })
 
@@ -185,16 +195,156 @@ const deal = (cardArr) => {
         playerTurn = false;
 
         //Deal Dealer Cards
-        if (!playerTurn){
+        if (!playerTurn) {
             dealerCardOne.src = cardTwo.imgUp;
-            if (count === 0) {
-                dealerCardThree = cardFive.imgup
+            dealerHand.innerHTML = `Dealer Hand: ${dealerScore}`
+            if (count === 0 && dealerScore < 16) {
+                setTimeout(() => {
+                    dealerCardThree.src = cardFive.imgUp;
+                    dealerCardThree.style.display = 'block';
+                }, 500);
+                dealerScore += cardFive.value;
+                if (dealerScore < 16) {
+                    setTimeout(() => {
+                        dealerCardFour.src = cardSix.imgUp;
+                        dealerCardFour.style.display = 'block';
+                    }, 1000);
+                    dealerScore += cardSix.value;
+                    if (dealerScore < 16) {
+                        setTimeout(() => {
+                            dealerCardFive.src = cardSeven.imgUp;
+                            dealerCardFive.style.display = 'block';
+                        }, 2000);
+                        dealerScore += cardSeven.value;
+                        if (dealerScore < 16) {
+                            setTimeout(() => {
+                                dealerCardSix.src = cardEight.imgUp;
+                                dealerCardSix.style.display = 'block';
+                            }, 3000);
+                            dealerScore += cardEight.value;
+                        }
+                    }
+                }
+            } else if (count === 1 && dealerScore < 16) {
+                setTimeout(() => {
+                    dealerCardThree.src = cardSix.imgUp;
+                    dealerCardThree.style.display = 'block';
+                }, 500);
+                dealerScore += cardSix.value;
+                if (dealerScore < 16) {
+                    setTimeout(() => {
+                        dealerCardFour.src = cardSeven.imgUp;
+                        dealerCardFour.style.display = 'block';
+                    }, 1000);
+                    dealerScore += cardSeven.value;
+                    if (dealerScore < 16) {
+                        setTimeout(() => {
+                            dealerCardFive.src = cardEight.imgUp;
+                            dealerCardFive.style.display = 'block';
+                        }, 2000);
+                        dealerScore += cardEight.value;
+                        if (dealerScore < 16) {
+                            setTimeout(() => {
+                                dealerCardSix.src = cardNine.imgUp;
+                                dealerCardSix.style.display = 'block';
+                            }, 3000);
+                            dealerScore += cardNine.value;
+                        }
+                    }
+
+                }
+            } else if (count === 2 && dealerScore < 16) {
+                setTimeout(() => {
+                    dealerCardThree.src = cardSeven.imgUp;
+                    dealerCardThree.style.display = 'block';
+                }, 500);
+                dealerScore += cardSeven.value;
+                if (dealerScore < 16) {
+                    setTimeout(() => {
+                        dealerCardFour.src = cardEight.imgUp;
+                        dealerCardFour.style.display = 'block';
+                    }, 1000);
+                    dealerScore += cardEight.value;
+                    if (dealerScore < 16) {
+                        setTimeout(() => {
+                            dealerCardFive.src = cardNine.imgUp;
+                            dealerCardFive.style.display = 'block';
+                        }, 2000);
+                        dealerScore += cardNine.value;
+                        if (dealerScore < 16) {
+                            setTimeout(() => {
+                                dealerCardSix.src = cardTen.imgUp;
+                                dealerCardSix.style.display = 'block';                                
+                            }, 3000);
+                            dealerScore += cardTen.value;
+                        }
+                    }
+
+                }
+            } else if (count === 3 && dealerScore < 16) {
+                setTimeout(() => {
+                    dealerCardThree.src = cardEight.imgUp;
+                    dealerCardThree.style.display = 'block';
+                }, 500);
+                dealerScore += cardEight.value;
+                if (dealerScore < 16) {
+                    setTimeout(() => {
+                        dealerCardFour.src = cardNine.imgUp;
+                        dealerCardFour.style.display = 'block';
+                    }, 1000);
+                    dealerScore += cardNine.value;
+                    if (dealerScore < 16) {
+                        setTimeout(() => {
+                            dealerCardFive.src = cardTen.imgUp;
+                            dealerCardFive.style.display = 'block';
+                        }, 2000);
+                        dealerScore += cardTen.value;
+                        if (dealerScore < 16) {
+                            setTimeout(() => {
+                                dealerCardSix.src = cardEleven.imgUp;
+                                dealerCardSix.style.display = 'block';
+                                
+                            }, 3000);
+                            dealerScore += cardEleven.value;
+                        }
+                    }
+
+                }
+            } else if (count === 4 && dealerScore < 16) {
+                setTimeout(() => {
+                    dealerCardThree.src = cardNine.imgUp;
+                    dealerCardThree.style.display = 'block';
+                }, 500);
+                dealerScore += cardNine.value;
+                if (dealerScore < 16) {
+                    setTimeout(() => {
+                        dealerCardFour.src = cardTen.imgUp;
+                        dealerCardFour.style.display = 'block';
+                    }, 1000);
+                    dealerScore += cardTen.value;
+                    if (dealerScore < 16) {
+                        setTimeout(() => {
+                            dealerCardFive.src = cardEleven.imgUp;
+                            dealerCardFive.style.display = 'block';
+                        }, 2000);
+                        dealerScore += cardEleven.value;
+                        if (dealerScore < 16) {
+                            setTimeout(() => {
+                                dealerCardSix.src = cardTwelve.imgUp;
+                                dealerCardSix.style.display = 'block';
+                                
+                            }, 3000);
+                            dealerScore += cardTwelve.value;
+                        }
+                    }
+
+                }
             }
         }
     })
 
-    
-    
+
+
 
 }
 
